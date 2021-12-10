2a0a3ab803e55ebbdcf625598b3e77ffd1ee7284
Becky Newton <iambexnewton@gmail.com>
2021-12-10 15:50:01 +0100

    rendering to webpage ok

--- src/App.js
+++ src/App.js
@@ -1,34 +1,75 @@
-import logo from "./logo.svg";
 import "./App.css";
+import { useEffect, useState } from "react";
 
 const SPACE_ID = "j6zm7o6y974w";
-const ACCESS_TOKEN = " 63jHrxO7iujkfejFc86ScaF8Pi94jgFkv_A_eqmxLLw";
+const ACCESS_TOKEN = "63jHrxO7iujkfejFc86ScaF8Pi94jgFkv_A_eqmxLLw";
 
-async function fetchData() {
+const query = `{
+  blogPostCollection(limit: 3) {
+    items {
+      title
+      richText {
+        json
+        links {
+          entries {
+            inline {
+              sys {
+                id
+              }
+          
+              
+              ... on BlogPost {
+                title
+              }
+            }
+            block {
+              sys{
+                id
+              }
+              ...on BlogPost {
+                title
+              }
+            }
+          }
+          
+        }
+      }
+    }
+    
+  }
+}`;
+
+async function fetchBlogPosts() {
+  // debugger;
+  // const body1 = JSON.stringify({ query });
+
   const response = await fetch(
-    "https://graphql.contentful.com/content/v1/spaces/${SPACE_ID}/environments/master",
+    `https://graphql.contentful.com/content/v1/spaces/${SPACE_ID}/environments/master`,
+    {
+      headers: {
+        Authorization: `Bearer ${ACCESS_TOKEN}`,
+        "Content-Type": "application/json",
+      },
+      method: "POST",
+      body: JSON.stringify({ query }),
+    },
   );
   const body = await response.json();
-  console.log(body);
+  return body.data.blogPostCollection.items[0];
 }
 
 function App() {
+  const [post, setPost] = useState(null);
+
+  useEffect(() => {
+    fetchBlogPosts().then((data) => {
+      setPost(data);
+    });
+  }, [setPost]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="App-header">Practice {post && post.title}</header>
     </div>
   );
 }
